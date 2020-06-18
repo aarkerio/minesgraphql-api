a0005ac55a82ef369e850b0b62c7d3b6504bf57b
aarkerio <mmontoya@gmail.com>
2020-06-18 14:49:24 -0500

    Adding TS support

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,5 +9,17 @@ module Types
     def test_field
       "Hello World!"
     end
+
+    # First describe the field signature:
+    field :post, PostType, null: true do
+      description "Find a post by ID"
+      argument :id, ID, required: true
+    end
+
+    # Then provide an implementation:
+    def post(id:)
+      Post.find(id)
+    end
+
   end
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
-const { environment } = require('@rails/webpacker')
+const { environment } =  require('@rails/webpacker');
+const typescript      =  require('./loaders/typescript');
+const sass            =  require('./loaders/sass');
 
-module.exports = environment
+const webpack = require("webpack");
+
+environment.loaders.prepend('typescript', typescript);
+
+environment.loaders.prepend('sass', sass);
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+ jQuery: 'jquery',
+ Popper: ['popper.js', 'default']
+}));
+
+module.exports = environment;
+
+
